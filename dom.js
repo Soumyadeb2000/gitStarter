8de655f8bb4837894e374a65c16adfe84e8ed79a
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-04-05 01:09:25 +0530

    querySelector

--- dom.js
+++ dom.js
@@ -1,4 +1,13 @@
-var item = document.getElementsByClassName('list-group-item')
-item[4].style.color = 'orange';
-var moreItem = document.getElementsByTagName('li');
-moreItem[4].style.color = 'orange';
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.background = 'green';
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.color = 'transparent';
+
+var secItem = document.querySelectorAll('li');
+secItem[1].style.color = 'green';
+
+var items = document.querySelectorAll('li:nth-child(odd)');
+for(let i=0 ; i<items.length ; i++)
+{
+items[i].style.background = 'green';
+}

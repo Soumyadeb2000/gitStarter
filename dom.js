7674a8b3131b18cc36d1c8abcaca34a4e48276c9
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-04-04 23:51:53 +0530

    adding second file

---
+++ dom.js
@@ -0,0 +1,5 @@
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+
+var items = document.getElementsByClassName('title');
+items[0].style.fontWeight = 'bold';

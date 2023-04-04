0bb5698852bf56ee700520aaf6d51850799d8dcb
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-04-05 00:12:39 +0530

    third file

--- dom.js
+++ dom.js
@@ -1,5 +1,6 @@
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
-
-var items = document.getElementsByClassName('title');
-items[0].style.fontWeight = 'bold';
+var list = document.getElementsByClassName('list-group-item');
+list[1].style.background = 'green';
+for(let i = 0;i <= list.length; i++)
+{
+    list[i].style.fontWeight = 'bold';
+}

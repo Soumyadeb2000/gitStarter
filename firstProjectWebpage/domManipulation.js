5cae828d0ca9287e0c8f2d1a319533bd9abea575
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-04-09 15:58:25 +0530

    update 1

--- firstProjectWebpage/domManipulation.js
+++ firstProjectWebpage/domManipulation.js
@@ -1,6 +1,12 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 var filter = document.getElementById('filter');
+var addDescription = document.createElement('input');
+addDescription.type = 'text';
+addDescription.className = 'form-control mr-2';
+addDescription.id = 'description';
+var submit = form.lastElementChild;
+form.insertBefore(addDescription, submit);
 
 form.addEventListener('submit', addItem);
 itemList.addEventListener('click', deleteItem);
@@ -11,7 +17,9 @@ function addItem(e){
     var newItem = document.getElementById('item').value;
     var li = document.createElement('li');
     li.className = 'list-group-item';
+    var descriptionText = document.getElementById('description').value;
     li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode('    '+descriptionText));
     var deleteBtn = document.createElement('button');
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
     deleteBtn.appendChild(document.createTextNode('X'));
@@ -37,7 +45,8 @@ function filterItem(e){
     var items = itemList.getElementsByTagName('li');
     Array.from(items).forEach(function(item){
         var itemName = item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text) != -1){
+        var description = item.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1){
             item.style.display = 'block';
         }
         else{
